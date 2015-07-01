65606b8558cdd40985285653e6f0942723495ba8
sudao <sudao@fun.tv>
2015-07-01 16:13:58 +0800

    init code.

---
+++ Assets/script/kick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kick : MonoBehaviour {
+	private bool kicked = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		if (!kicked) {
+			kicked = true;
+			print ("OnMouseDown");
+			rigidbody2D.AddForce (new Vector2 (-100, 20));
+		}
+	}
+}
